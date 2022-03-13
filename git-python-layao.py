4fbfdf612b3fd33639ac44293644a51caa8b62c9
LayaoCharleneJoy <layaocharlenejoy@gmail.com>
2022-03-13 22:59:53 +0700

    My Educational Background

--- git-python-layao.py
+++ git-python-layao.py
@@ -7,4 +7,7 @@ Created on Sun Mar 13 22:19:56 2022
 
 print ("Charlene Joy L. layao");
 print ("Buliasnin, Boac, Marinduque");
-print ("November 6, 2001");
+print ("November 6, 2001"); 
+print ("Marinduque State College");
+print ("Bachelor of Science in Information Technology");
+print ("2B");
