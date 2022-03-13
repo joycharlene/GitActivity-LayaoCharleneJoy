86408467f59cb7cdc504d334ba2405419df5a562
LayaoCharleneJoy <layaocharlenejoy@gmail.com>
2022-03-13 22:36:00 +0700

    My Personal Info added

---
+++ git-python-layao.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 13 22:19:56 2022
+
+@author: Ceejhay
+"""
+
+print ("Charlene Joy L. layao");
+print ("Buliasnin, Boac, Marinduque");
+print ("November 6, 2001");
